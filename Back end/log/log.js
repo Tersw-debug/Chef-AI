f0d437e1e8e658aa5633cf940b5bbc333c830bde
XCticon <Azusagawa.sakuta.m@gmail.com>
2025-12-06 20:46:25 +0200

    Log midelware Added

---
+++ Back end/log/log.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const {v4: uuid} = require('uuid');
+const filePromise = require('fs/promises');
+const file = require('fs');
+const {format} = require('date-fns');
+
+
+
+const logEvents = async (message, logName) =>{
+    const time = `${format(new Date(),'yyyy-MM-dd\tHH-mm-ss')}`;
+    const log = `${message}\t${uuid()}\t${time}\n`;
+    try{
+        if(!file.existsSync(path.join(__dirname,'..','log')))
+            await filePromise.mkdir(path.join(__dirname, '..','log'));
+
+        await filePromise.appendFile(path.join(__dirname,'..','log',logName) ,log);
+    }
+    catch(e){
+        console.log(e)
+    }
+
+
+}
+
+const logger = (req,res,next) =>{
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'log.txt');
+    console.log(`${req.method}\t${req.path}`);
+    next();
+}
+
+
+module.exports = { logger , logEvents }
